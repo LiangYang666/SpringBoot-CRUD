b3b462a15c83b737757cfc608c7c7caa27659195
LiangYang <mail.liangyang@gmail.com>
2022-05-25 10:15:58 +0800

    完成maven项目创建 并写一个测试

---
+++ src/main/java/com/liang/MyTest.java
@@ -0,0 +1,12 @@
+package com.liang;
+
+/**
+ * @Description: TODO
+ * @Author: LiangYang
+ * @Date: 2022/5/25 上午10:04
+ **/
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
