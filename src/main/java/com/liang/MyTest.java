d240230753a5034f7f224a127d86f7803a4b225b
LiangYang <mail.liangyang@gmail.com>
2022-05-25 11:10:43 +0800

    改写main函数

--- src/main/java/com/liang/MyTest.java
+++ src/main/java/com/liang/MyTest.java
@@ -1,12 +1,16 @@
 package com.liang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description: TODO
  * @Author: LiangYang
  * @Date: 2022/5/25 上午10:04
  **/
+@SpringBootApplication
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        SpringApplication.run(MyTest.class, args);
     }
 }
