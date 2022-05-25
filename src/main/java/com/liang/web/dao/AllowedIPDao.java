9c2e58d7b8c61bb809b82047f708b376d94ed9de
LiangYang <mail.liangyang@gmail.com>
2022-05-25 16:14:58 +0800

    dao

---
+++ src/main/java/com/liang/web/dao/AllowedIPDao.java
@@ -0,0 +1,10 @@
+package com.liang.web.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liang.web.entity.AllowedIpEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AllowedIPDao extends BaseMapper<AllowedIpEntity> {
+
+}
