29cf63d77e7a4679fd6bba659df52700feec45b3
LiangYang <mail.liangyang@gmail.com>
2022-05-25 16:15:25 +0800

    添加扫描

--- src/main/java/com/liang/MyTest.java
+++ src/main/java/com/liang/MyTest.java
@@ -1,5 +1,6 @@
 package com.liang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2022/5/25 上午10:04
  **/
 @SpringBootApplication
+@MapperScan("com.liang.web.dao")
 public class MyTest {
     public static void main(String[] args) {
         SpringApplication.run(MyTest.class, args);
     }
+
+
+
 }
